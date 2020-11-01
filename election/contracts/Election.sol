7159f387fffd138d1c6c91c70194ed0e57211f51
Jennifer Fadriquela <jenniferfadriquela@gmail.com>
2020-11-01 09:38:29 +0800

    intial commit

---
+++ election/contracts/Election.sol
@@ -0,0 +1,49 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Election {
+    // Model a candidate
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+
+    // Read/write Candidates
+    mapping(uint256 => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint256 public candidatesCount;
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
+    event votedEvent(uint256 indexed _candidateId);
+
+    // Constructor
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint256 _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+}
