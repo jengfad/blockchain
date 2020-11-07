bdb709aafd9fe29a0354b1377455fce28b31e267
Jennifer Fadriquela <jenniferfadriquela@gmail.com>
2020-11-08 04:05:01 +0800

    completed main and token contracts

--- SupplyChain/contracts/SupplyChain.sol
+++ SupplyChain/contracts/SupplyChain.sol
@@ -31,4 +31,140 @@ contract supplyChain {
     }
     mapping(uint32 => registration) public registrations; // Registrations by Registration ID (r_id)
     mapping(uint32 => uint32[]) public productTrack; // Registrations by Product ID (p_id) / Movement track for a product
+
+    event Transfer(uint32 productId);
+
+    // start of: participant functions
+
+    function createParticipant(
+        string memory _name,
+        string memory _pass,
+        address _pAdd,
+        string memory _pType
+    ) public returns (uint32) {
+        uint32 userId = u_id++;
+        participants[userId].userName = _name;
+        participants[userId].password = _pass;
+        participants[userId].participantAddress = _pAdd;
+        participants[userId].participantType = _pType;
+        return userId;
+    }
+
+    function getParticipantDetails(uint32 _p_id)
+        public
+        view
+        returns (
+            string memory,
+            address,
+            string memory
+        )
+    {
+        return (
+            participants[_p_id].userName,
+            participants[_p_id].participantAddress,
+            participants[_p_id].participantType
+        );
+    }
+
+    // end of: participant functions
+
+    // start of: product functions
+
+    function createProduct(
+        uint32 _ownerId,
+        string memory _modelNumber,
+        string memory _partNumber,
+        string memory _serialNumber,
+        uint32 _productCost
+    ) public returns (uint32) {
+        if (
+            keccak256(
+                abi.encodePacked(participants[_ownerId].participantType)
+            ) == keccak256("Manufacturer")
+        ) {
+            uint32 productId = p_id++;
+            products[productId].modelNumber = _modelNumber;
+            products[productId].partNumber = _partNumber;
+            products[productId].serialNumber = _serialNumber;
+            products[productId].cost = _productCost;
+            products[productId].productOwner = participants[_ownerId]
+                .participantAddress;
+            products[productId].mfgTimeStamp = uint32(block.timestamp);
+            return productId;
+        }
+        return 0;
+    }
+
+    modifier onlyOwner(uint32 _productId) {
+        require(msg.sender == products[_productId].productOwner);
+        _;
+    }
+
+    function getProductDetails(uint32 _productId)
+        public
+        view
+        returns (
+            string memory,
+            string memory,
+            string memory,
+            uint32,
+            address,
+            uint32
+        )
+    {
+        return (
+            products[_productId].modelNumber,
+            products[_productId].partNumber,
+            products[_productId].serialNumber,
+            products[_productId].cost,
+            products[_productId].productOwner,
+            products[_productId].mfgTimeStamp
+        );
+    }
+
+    // end of: product functions
+
+    // start of: supply chain movement functions
+
+    function transferToOwner(
+        uint32 _user1Id,
+        uint32 _user2Id,
+        uint32 _prodId
+    ) public onlyOwner(_prodId) returns (bool) {
+        participant memory p1 = participants[_user1Id];
+        participant memory p2 = participants[_user2Id];
+        uint32 registration_id = r_id++;
+        registrations[registration_id].productId = _prodId;
+        registrations[registration_id].productOwner = p2.participantAddress;
+        registrations[registration_id].ownerId = _user2Id;
+        registrations[registration_id].trxTimeStamp = uint32(block.timestamp);
+        products[_prodId].productOwner = p2.participantAddress;
+        productTrack[_prodId].push(registration_id);
+        emit Transfer(_prodId);
+        return (true);
+    }
+
+    function getProductTrack(uint32 _prodId)
+        external
+        view
+        returns (uint32[] memory)
+    {
+        return productTrack[_prodId];
+    }
+
+    function getRegistrationDetails(uint32 _regId)
+        public
+        view
+        returns (
+            uint32,
+            uint32,
+            address,
+            uint32
+        )
+    {
+        registration memory r = registrations[_regId];
+        return (r.productId, r.ownerId, r.productOwner, r.trxTimeStamp);
+    }
+
+    // end of: supply chain movement functions
 }
--- SupplyChain/contracts/erc20Token.sol
+++ SupplyChain/contracts/erc20Token.sol
@@ -14,5 +14,57 @@ abstract contract ERC20Token is ERC20Interface {
     uint8 public decimals; // How many decimals to use to display amounts
     string public symbol; // Short identifier for token (i.e. FDT)
 
-    uint256 private _totalSupply; // Total number of tokens
+    uint256 private tokenTotalSupply; // Total number of tokens
+
+    constructor(
+        uint256 _initialAmount,
+        string memory _tokenName,
+        uint8 _decimalUnits,
+        string memory _tokenSymbol
+    ) {
+        balances[msg.sender] = _initialAmount; // The creator owns all tokens
+        tokenTotalSupply = _initialAmount; // Update total token supply
+        name = _tokenName; // Token name
+        decimals = _decimalUnits; // Number of decimals
+        symbol = _tokenSymbol; // Token symbol
+    }
+
+    function transfer(address _to, uint256 _value) public override returns (bool success) {
+        require(_value >= 0,"Cannot transfer negative amount.");
+        require(balances[msg.sender] >= _value,"Insufficient funds.");
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public override returns (bool success) {
+        uint256 allowance = allowed[_from][msg.sender];
+        require(balances[_from] >= _value && allowance >= _value,"Insufficient funds.");
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        if (allowance < MAX_UINT256) {
+            allowed[_from][msg.sender] -= _value;
+        }
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) public override view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) public override returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public override view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+    function totalSupply() public override view returns (uint256 totSupp) {
+        return tokenTotalSupply;
+    }
 }
