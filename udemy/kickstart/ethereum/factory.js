78569f2ce6492762d7606ab39300683610d96bf8
Jennifer Fadriquela <jenniferfadriquela@gmail.com>
2020-12-20 09:50:40 +0800

    add pages for: contribute, show campaign

--- udemy/kickstart/ethereum/factory.js
+++ udemy/kickstart/ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     CampaignFactory.abi,
-    '0x6DE9E91fa5bd08d8b36eFCACa3F954a7809cB4EA'
+    '0x458D4C73D5066f4F59c5dc2Fe44115fE7346555b'
 );
 
 export default instance;
